e3cea54f3e92f20ee7012998dcca64d36143d4b6
Mike Hill <mike@mikehillvideo.com>
2016-12-20 07:37:41 -0800

    Updates: Fixed paths for new and existing file templates

--- private/templates/js/model.js
+++ private/templates/js/model.js
@@ -1,29 +1,29 @@
 // { "path" : "imports/models/__modelName__/__modelName__.js" }
 
-const __modelName__ = new Mongo.Collection('__modelName__');
+const __modelName__ = new Mongo.Collection('__modelName__'.toLowerCase());
 
 __modelName__.attachSchema(
     new SimpleSchema({
-    title: {
-      type: String
-    },
-    content: {
-      type: String
-    },
-    createdAt: {
-      type: Date,
-      denyUpdate: true
-    }
-  })
+        title: {
+            type: String
+        },
+        content: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            denyUpdate: true
+        }
+    })
 );
 
 // Collection2 already does schema checking
 if (Meteor.isServer) {
-  __modelName__.allow({
-    insert : () => false,
-    update : () => false,
-    remove : () => false
-  });
+    __modelName__.allow({
+        insert : () => false,
+        update : () => false,
+        remove : () => false
+    });
 }
 
 export default __modelName__;
