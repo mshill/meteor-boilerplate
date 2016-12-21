6c75c40fab913195f437b98d970bc915ae52ab68
Mike Hill <mike@mikehillvideo.com>
2016-12-21 14:00:05 -0800

    Added model autoimport functionality in /server/entry.js

--- private/templates/js/publication.js
+++ private/templates/js/publication.js
@@ -1,4 +1,6 @@
 // { "path" : "imports/models/__modelName__/server/publications.js" }
+import __modelName__ from '../__modelName__';
+
 // TODO: call this in entry file
 export default function () {
     Meteor.publish('__collectionName__', function () {
--- server/entry.js
+++ server/entry.js
@@ -2,9 +2,12 @@ import setupBrowserPolicy from './config/security.js';
 import loadFixtures from './loaders/fixtures.js';
 import loadUsers from './loaders/users.js';
 
+// AUTOIMPORTS
+// ^^^^^^^^^^^ DO NOT REMOVE!!!
+
 setupBrowserPolicy(BrowserPolicy);
 
 Meteor.startup(() => {
-  loadUsers();
-  //loadFixtures([{ foo: 'bar' }], myCollection);
+    loadUsers();
+    //loadFixtures([{ foo: 'bar' }], myCollection);
 });
